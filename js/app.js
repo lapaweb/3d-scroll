4924b2c07372777161f538abc22ee4fe15c97b70
lapaweb <lapa-34@ya.ru>
2023-01-09 21:48:44 +0300

    feature: audio content

--- js/app.js
+++ js/app.js
@@ -25,3 +25,21 @@ window.onscroll = function() {
 }
 
 window.scrollTo(0, 1)
+
+//Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+  audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+  soundButton.classList.toggle('paused')
+  audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+  soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+  audio.pause()
+}
